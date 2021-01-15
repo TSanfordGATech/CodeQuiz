d863fffd3dad567b624f1d7ce256c49e22102c86
Tori Sanford <tori.danielle.clark91@gmail.com>
2021-01-14 22:00:41 -0500

    final commit with final approval save

--- Javafiles/score.js
+++ Javafiles/score.js
@@ -1,3 +1,5 @@
+// This sheet is just for the Scores section
+
 function printHighscores() {
     // either get scores from localstorage or set to empty array
     var highscores = JSON.parse(window.localStorage.getItem("highscores")) || [];
